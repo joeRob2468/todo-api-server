b061d3e863e81e072363588031e3d3db6c803994
John Boyer <joeRob2468@yahoo.com>
2018-03-06 14:03:07 -0600

    removed build paths from git

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _server = require("./server");
-
-var _server2 = _interopRequireDefault(_server);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
