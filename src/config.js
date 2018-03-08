9292d4667decbf81aa75ccf6c9baaeb0d370a0b4
John Boyer <joeRob2468@yahoo.com>
2018-03-08 12:44:45 -0600

    added dotenv, restructured project a bit

--- src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/todo-api',
-  port: process.env.PORT || 8080,
-};
-
-export default config;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-import server from "./server";
+'use strict';
+
+import Express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+// initialize the express app
+const app = new Express();
+app.use(cors());
+
+// connect to database
+
+// apply body parser and server public assets and routes
+app.use(bodyParser.json({ limit: process.env.BODY_LIMIT }));
+app.use(bodyParser.urlencoded({ limit: process.env.BODY_LIMIT, extended: false }));
+
+// start app
+app.listen(process.env.PORT, (err) => {
+  if (!err) {
+    console.log(`Server is running on port ${process.env.PORT}.`);
+  }
+});
+
+export default app;
