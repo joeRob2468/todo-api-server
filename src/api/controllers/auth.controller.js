265ba5f2cfc23ce729b84f8424402a6121a4990b
John Boyer <joeRob2468@yahoo.com>
2018-03-27 13:25:43 -0500

    added auth controller

---
+++ src/api/controllers/auth.controller.js
@@ -0,0 +1,85 @@
+import httpStatus from 'http-status';
+import User from '../models/user.model';
+import RefreshToken from '../models/refreshToken.model';
+import moment from 'moment-timezone';
+
+/** 
+ * returns a formatted object with tokens
+ * @private
+ */
+function generateTokenResponse(user, accessToken) {
+  const tokenType = 'Bearer';
+  const refreshToken = RefreshToken.generate(user).token;
+  const expiresIn = moment().add(process.env.JWT_EXPIRATION_MINUTES);
+  return {
+    tokenType, accessToken, refreshToken, expiresIn
+  };
+}
+
+/** 
+ * returns jwt token if registration was successful
+ * @public
+ */
+export const register = async (req, res, next) => {
+  try {
+    const user = await (new User(req.body)).save();
+    const userTransformed = user.transform();
+    const token = generateTokenResponse(user, user.token());
+
+    res.status(httpStatus.CREATED);
+    return res.json({ token, user: userTransformed });
+  } catch (error) {
+    return next(User.checkDuplicateEmail(error));
+  }
+};
+
+/**
+ * returns jwt token if valid username and password is provided
+ * @public
+ */
+export const login = async (req, res, next) => {
+  try {
+    const { user, accessToken } = await User.findAndGenerateToken(req.body);
+    const token = generateTokenResponse(user, accessToken);
+    const userTransformed = user.transform();
+    return res.json({ token, user: userTransformed });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+/**
+ * login with an existing user or creates a new one if valid accessToken token
+ * Returns jwt token
+ * @public
+ */
+export const oAuth = async (req, res, next) => {
+  try {
+    const { user } = req;
+    const accessToken = user.token();
+    const token = generateTokenResponse(user, accessToken);
+    const userTransformed = user.transform();
+    return res.json({ token, user: userTransformed });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+/** 
+ * returns a new jwt when given a valid refresh token
+ * @public
+ */
+export const refresh = async(req, res, next) => {
+  try {
+    const { email, refreshToken } = req.body;
+    const refreshObject = await RefreshToken.findOneAndRemove({
+      userEmail: email,
+      token: refreshToken
+    });
+    const { user, accessToken } = await User.findAndGenerateToken({ email, refreshObject });
+    const response = generateTokenResponse(user, accessToken);
+    return res.json(response);
+  } catch (error) {
+    return next(error);
+  }
+};
